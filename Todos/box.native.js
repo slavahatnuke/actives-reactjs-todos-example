55e05dc328e822234d22e09d5aeff5988ad4def3
slava hatnuke <slava.hatnuke@gmail.com>
2016-07-27 22:44:30 +0300

    todos

---
+++ Todos/box.native.js
@@ -0,0 +1,12 @@
+import box from './box';
+//
+// box.add('Todo', ({TodoState}) => connect(TodoState)(Todo()));
+// box.add('TodoList', ({Todo, TodoListState}) => connect(TodoListState)(TodoList({Todo})));
+// box.add('TodoEditor', ({TodoEditorState}) => connect(TodoEditorState)(TodoEditor()));
+// box.add('TodoApp', TodoApp); ????
+
+
+export default box;
+
+
+
